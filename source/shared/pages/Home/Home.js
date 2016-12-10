e500685da7a58175669e1297628150034385c0d2
Luca Barone <baro.luc@gmail.com>
2016-12-10 10:40:21 +0100

    :sparkles: first working draft of the todo list

--- source/shared/pages/Home/Home.js
+++ source/shared/pages/Home/Home.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
+import {compose, withState, withProps} from 'recompose'
 
-const Component = () => <div>home</div>
+const enhance = compose(
+  withState('value', 'setValue', ''),
+  withState('todos', 'updateTodos', ['foo', 'bar']),
+  withProps(({todos, updateTodos, value, setValue}) => ({
+    handleSubmit: e => (
+      e.preventDefault(),
+      updateTodos([...todos, value]),
+      setValue('')
+    ),
+    handleChange: e => setValue(e.target.value),
+  }))
+)
+
+const Component = ({todos, value, handleChange, handleSubmit}) =>
+  <main>
+    {todos.map((item, i) =>
+      <div key={i}>{item}</div>
+    )}
+
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+      />
+      <input type="submit" value="add"/>
+    </form>
+  </main>
 
 Component.displayName = 'HomePage'
+Component.propTypes = {
+  todos: PropTypes.array.isRequired,
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+}
 
-export default Component
+export default enhance(Component)
