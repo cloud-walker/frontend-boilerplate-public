5282d98a93aa4567811315e8a3775067b3813449
Luca Barone <baro.luc@gmail.com>
2016-12-19 08:15:36 +0100

    :white_check_mark: 100 coverage for the TodoList component

--- source/shared/components/TodoList/TodoList.test.js
+++ source/shared/components/TodoList/TodoList.test.js
@@ -2,6 +2,15 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import TodoList from './index'
 
-test('<TodoList/>', () => {
+test('<TodoList/> empty', () => {
   expect(shallow(<TodoList todos={[]}/>)).toMatchSnapshot()
+})
+
+test('<TodoList/> with some todos', () => {
+  const wrapper = shallow(<TodoList todos={[
+    'Clean the Kitchen',
+    'Go out for a walk',
+  ]}/>)
+
+  expect(wrapper).toMatchSnapshot()
 })
