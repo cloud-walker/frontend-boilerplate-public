1de8a553fdd4c495831daf093f9a5a0cd4376d2a
Luca Barone <baro.luc@gmail.com>
2016-12-25 07:37:21 +0100

    Finally 100 coverage, closes #11

--- source/shared/components/FormNewTodo/FormNewTodo.test.js
+++ source/shared/components/FormNewTodo/FormNewTodo.test.js
@@ -2,8 +2,22 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import FormNewTodo from './index'
 
-test('it should render properly', () => {
-  const wrapper = shallow(<FormNewTodo value="prova"/>)
+test('<FormNewTodo/>', () => {
+  const preventDefault = jest.fn()
+  const addTodo = jest.fn()
+  const subject = shallow(
+    <FormNewTodo
+      addTodo={addTodo}
+    />
+  )
 
-  expect(wrapper).toMatchSnapshot()
+  subject.dive()
+    .find('[type="text"]')
+    .simulate('change', {target: {value: 'woot'}})
+
+  subject.dive()
+    .simulate('submit', {preventDefault})
+
+  expect(preventDefault).toHaveBeenCalled()
+  expect(addTodo).toHaveBeenCalled()
 })
